73f47d12e10afe5a8141f19ccc8369a1a0ee42a4
Mauricio Mani <mauricioman12@gmail.com>
2021-01-06 09:34:55 -0600

    Improvements to home and LP added

--- webpage.py
+++ webpage.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from trends import main
 import json
 import plotly
+import lp
 
 app = Flask(__name__)
 
@@ -30,6 +31,24 @@ def blog():
 def blog_lim_problem():
     return(render_template("blog/lim_problem.html"))
 
+@app.route("/blog/sample")
+def blog_sample():
+    return(render_template("blog/blog_sample.html"))
+
+@app.route("/working_schedule", methods = ["GET"])
+def working_schedule():
+    group_one = int(request.args.get('groupOne'))
+    group_two = int(request.args.get("groupTwo"))
+    group_three = int(request.args.get("groupThree"))
+    group_four = int(request.args.get("groupFour"))
+    group_five = int(request.args.get("groupFive"))
+    group_six = int(request.args.get("groupSix"))
+    opt_x = lp.simple_working_schedule(group_one, group_two, group_three, group_four, group_five, group_six)
+    return(render_template("lp_simple_solution.html", opt_x = list(map(round, opt_x))))
+
+@app.route("/working_schedule_form")
+def working_schedule_form():
+    return(render_template("working_schedule_form.html"))
 
 if __name__=="__main__":
     app.run(debug=False)
